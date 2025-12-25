bafae9426e2512653e58fe213ca9817e37f19a85
MohamadMajed-git <mohammadcoc1290@gmail.com>
2025-12-25 20:50:23 +0200

    حل مشكلة التعارض في الملفات

--- cppBack/test.cpp
+++ cppBack/test.cpp
@@ -1,24 +1,25 @@
-// #include <iostream>
-// #include <time.h>
-// #include <iomanip>
-// #include <sstream> 
-// using namespace std;
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <sstream> 
+using namespace std;
 
-// int main() {
-//         //     srand(time(0));
-//         // string token="";
-//         // for(int i=0;i<26;i++){
-//         //     token+=(char)('A'+ rand() % 26);
-//         // }
-//         // cout<<token<<endl;
+int main() {
+        //     srand(time(0));
+        // string token="";
+        // for(int i=0;i<26;i++){
+        //     token+=(char)('A'+ rand() % 26);
+        // }
+        // cout<<token<<endl;
 
-//         time_t t =time(NULL);
-//         tm date=*localtime(&t);
-//         stringstream ss;
-//         ss<<put_time(&date, "%Y-%m-%d %H:%M:%S");
-//         string finalDate=ss.str();
-//         cout<<finalDate<<endl;
+        time_t t =time(NULL);
+        tm date=*localtime(&t);
+        stringstream ss;
+        ss<<put_time(&date, "%Y-%m-%d %H:%M:%S");
+        string finalDate=ss.str();
+        cout<<finalDate<<endl;
+        
         
 
-//     return 0;
-// }
+    return 0;
+}
